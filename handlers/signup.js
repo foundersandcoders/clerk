00815ad2c689d9b1e76deb4002cb8a7804014a5e
FilWisher <williamfisher@hotmail.co.uk>
2015-04-15 16:39:50 +0100

    test commit

--- handlers/signup.js
+++ handlers/signup.js
@@ -7,12 +7,10 @@ function signup (req, res) {
   var url = "http://0.0.0.0:8000/register";
   var payload = JSON.stringify(req.payload);
 
-  console.log(url, payload);
   request.post({
     url: url,
     body: payload
   }, function (e, h, r) {
-    //console.log(h);
     res(r);
   });
 }
