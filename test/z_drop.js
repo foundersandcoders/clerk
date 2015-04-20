ef7ccff4bdff2736e96458dfad127a3c44045e8a
FilWisher <williamfisher@hotmail.co.uk>
2015-04-20 13:41:06 +0100

    add screens and basic styling

--- test/z_drop.js
+++ test/z_drop.js
@@ -2,16 +2,17 @@
 
 var http = require("http");
 
-var options = {
-  host:"127.0.0.1",
-  port: 9200, // use ENV var?
-  path: "/_all", // DELETEs EVERYTHING!!
-  method: "DELETE",
-  headers: { "Content-Type": "application/json" }
-};
 
 var drop = function(callback) {
-  
+
+  var options = {
+    host:"127.0.0.1",
+    port: 9200, // use ENV var?
+    path: "/_all", // DELETEs EVERYTHING!!
+    method: "DELETE",
+    headers: { "Content-Type": "application/json" }
+  };
+
   var req = http.request(options, function(res) {
     res.setEncoding("utf8");
     var resStr = "";
