3513b99c81e5d62d6dc196dffb6dba22da1fd7e9
besartshyti <besartshyti@MacBook-Air-di-Besart.local>
2015-04-23 20:16:11 +0100

    payment list member

--- handlers/showmember.js
+++ handlers/showmember.js
@@ -1,31 +1,63 @@
 "use strict";
 
-var request = require("request");
+var request    = require("request");
 var membersUrl = process.env.MEMBERS_URL || "http://0.0.0.0:8010";
-var clean = require("d-bap");
+var clean      = require("d-bap");
+var async      = require("async");
 
 function showMember (req, res) {
 
-  var opts = {
-    method: "GET",
-    url: membersUrl + "/members/" + req.params.id,
-    headers: {
-      authorization: req.auth.credentials.token
-    },
-    json: true
-  };
+    var optsMember = {
+        method: "GET",
+        url: membersUrl + "/members/" + req.params.id,
+        headers: {
+            authorization: req.auth.credentials.token
+        },
+        json: true
+    };
 
-  request(opts, function (e, h, r) {
+    var optsPayments = {
+        method: "GET",
+        url: membersUrl + "/payments?memberId=" + req.params.id,
+        headers: {
+            authorization: req.auth.credentials.token
+        },
+        json: true
+    };
 
-    var member;
-    if (e || (h.statusCode === 404) || !r) {
-      return res({ statusCode: 404, status: "Not found", message: "Member could not be found" }).code(404);
-    } else {
-      member = r._source;
-      member.id = r._id;
-      return res.view("showmember", { member: clean.object(member) });
-    }
-  });
+    async.parallel(
+        [
+            function (callback){
+                request(optsMember, function (e, h, r){
+                    if (e || (h.statusCode === 404) || !r) {
+                        callback(arguments, null);
+                    } else {
+                        callback(null, r);
+                    }
+                });
+            },
+            function (callback){
+                request(optsPayments, function (e, h, r){
+                    if (e || (h.statusCode === 404) || !r) {
+                        callback(arguments, null);
+                    } else {
+                        callback(null, r);
+                    }
+                });
+            }
+        ],
+        function (err, results){
+
+            if(err){
+                return res({ statusCode: 404, status: "Not found", message: "Member could not be found" }).code(404);
+            }else{
+                var member   = results[0]._source;
+                member.id    = results[0]._id;
+                var payments = results[1];
+                return res.view("showmember", { member: clean.object(member), payments: payments });
+            }
+        }
+    );
 }
 
 module.exports = showMember;
