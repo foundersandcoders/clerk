6f276302fb7244683305933e0c4ee62fbec7b63e
besartshyti <besartshyti@MacBook-Air-di-Besart.local>
2015-04-23 18:42:13 +0100

    payment view

---
+++ handlers/helpers.js
@@ -0,0 +1,13 @@
+var paymentTypes =  {
+    CHQ:   "cheque",
+    CASH:  "cash",
+    SOA:   "standing order payment advised (by member)",
+    SOR:   "standing order received (and shown on bank statement)",
+    BACSA: "BACS payment advised (by member)",
+    BACSR: "BACS payment received by bank (and shown on bank statement)",
+    CAFA:  "charities aid foundation payment advised by member",
+    CAFR:  "charities aid foundation payment received by bank (shown on bank statement)",
+    HO:    "payment received by Harbour Office along with harbour dues"
+};
+
+exports.validateTypes = Object.keys(paymentTypes);
