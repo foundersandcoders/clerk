8105700660b9231656d1471fbd6adfbe4a6f3b70
besartshyti <besartshyti@MacBook-Air-di-Besart.local>
2015-04-23 21:34:24 +0100

    edit payment

---
+++ handlers/showeditpayment.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var membersUrl = process.env.MEMBERS_URL || "http://0.0.0.0:8010";
+var request    = require("request");
+
+function showEditPayment (req, res) {
+
+
+    var opts = {
+        method: "GET",
+        url: membersUrl + "/payments/" + req.params.idPay,
+        json: true,
+        headers: {
+            authorization: req.auth.credentials.token
+        }
+    };
+
+    request(opts, function (e, h, r) {
+
+        if (e || h.statusCode === 404 || !r) {
+            return res(r).code(404);
+        } else {
+            var obj = r._source;
+            obj.id  = r._id;
+            return res.view("editpayment", {payment:obj});
+        }
+    });
+}
+
+module.exports = showEditPayment;
---
+++ test/showeditpayment.test.js
@@ -0,0 +1,107 @@
+"use strict";
+
+var test = require("tape");
+var server = require("../lib/server.js");
+var drop = require("./z_drop.js");
+var members = require("rubberbands")("clerk", "members");
+
+var cookie;
+
+test("Delete records", function(t) {
+  drop(function(res){
+    t.equal(res.acknowledged, true, "ALL Records DELETED!");
+    t.end();
+  }).end();
+});
+
+test("GET /members/{id} should return 302 if not logged in", function (t) {
+
+  var opts = {
+    method: "GET",
+    url: "/members/wil",
+  };
+
+  server.inject(opts, function (res) {
+
+    t.equals(res.statusCode, 302, "302 returned");
+    t.end();
+  });
+});
+
+test("POST /signup should create account and return cookie with 302", function (t) {
+
+  var payload = {
+    email: "wil",
+    password: "hello",
+    cpassword: "hello"
+  };
+  var request = {
+    method: "POST",
+    url: "/signup",
+    payload: payload
+  };
+
+  server.inject(request, function (res) {
+
+    t.equals(res.statusCode, 302, "302 returned");
+    t.ok(res.headers["set-cookie"], "cookie returned");
+    cookie = res.headers["set-cookie"][0].split(";")[0];
+    t.end();
+  });
+});
+
+
+test("create member", function (t) {
+
+  members.create({
+    id: 1234,
+    name: "wil"
+  }, function (res) {
+
+    t.ok(res.created, "member created");
+    t.end();
+  });
+});
+
+test("GET /members/1234 should return 200 if correct member id and valid session", function (t) {
+
+  var opts = {
+    method: "GET",
+    url: "/members/1234",
+    headers: {
+      cookie: cookie
+    }
+  };
+
+  server.inject(opts, function (res) {
+
+    t.equals(res.statusCode, 200, "200 returned");
+    t.end();
+  });
+});
+
+
+test("GET /members/1234 should return 404 if correct member id but non existing payment", function (t) {
+
+  var opts = {
+    method: "GET",
+    url: "/members/1234/payments/87593087",
+    headers: {
+      cookie: cookie
+    }
+  };
+
+  server.inject(opts, function (res) {
+
+    t.equals(res.statusCode, 404, "404 returned");
+    t.end();
+  });
+});
+
+
+test("Delete records", function(t) {
+  drop(function(res){
+    t.equal(res.acknowledged, true, "ALL Records DELETED!");
+    t.end();
+  }).end();
+});
