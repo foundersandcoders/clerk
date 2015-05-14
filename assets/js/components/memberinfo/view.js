0a4bc2d96e27dea47a3de16db9012982cfbfce74
besartshyti <besartshyti@gmail.com>
2015-05-14 14:37:33 +0100

    date payments and component show info member

---
+++ assets/js/components/memberinfo/view.js
@@ -0,0 +1,36 @@
+// "use strict";
+
+// var h = require("virtual-dom/h");
+
+// module.exports = function (data) {
+
+// 	return h("div.individual-section", [
+// 		renderPersonalInfo(data),
+// 		renderAddressInfo(data),
+// 		renderMembership(data)
+// 	]);
+
+// 	function renderPersonalInfo (data) {
+
+// 		return h("div.col-1", [
+// 			h("p", [
+// 				h("span.info", "Name"),
+// 				h()
+// 			])
+// 		]);
+// 	}
+
+// 	function renderAddressInfo (data) {
+
+// 		return h("div.col-2", [
+
+// 		]);
+// 	}
+
+// 	function renderMembership (data) {
+		
+// 		return h("div.col-3", [
+
+// 		]);
+// 	}
+// };
