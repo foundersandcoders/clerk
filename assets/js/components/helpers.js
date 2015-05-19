343e05853ed27524c88f0ea4e640e39a457fc3ba
besartshyti <besartshyti@gmail.com>
2015-05-19 18:30:24 +0100

    edit member

---
+++ assets/js/components/helpers.js
@@ -0,0 +1,23 @@
+var memberTypes = module.exports.memberTypes = [{
+		value: "annual-single",
+		description: "Annual Single"
+	}, {
+		value: "annual-double",
+		description: "Annual Double"
+	},{
+		value: "annual-family",
+		description: "Annual Family"
+	},{
+		value: "life-single",
+		description: "Life Single"
+	},{
+		value: "life-double",
+		description: "Life Double"
+	},{
+		value: "group-annual",
+		description: "Group Annual"
+	},{
+		value: "corporate-annual",
+		description: "Corporate Annual"
+	}
+];
